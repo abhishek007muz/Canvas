698dc06d7604976a570c0b03adf61a3ffda383cf
abhishek007muz <78969924+abhishek007muz@users.noreply.github.com>
2021-02-26 09:14:29 +0530

    Add files via upload
    Canvas using C#.Net provides 2D drawing surface for users. The aim of this project is to provide an interface to users for drawing, editing, and creating illustrations which are simple to detailed digital paintings and art objects in computer. The application can refer to distinctive visual elements that represents an individual artist’s work. This project has file menu which has options to open a new canvas. It also has an option to open an existing painting and edit the painting. It provides an option to save the art in the system’s local storage and we can exit the application with exit option. The toolbox which are arranged to the left, provides tools like pencil, eraser, line, square, circle, and text editing tool. The application also has a color tool to select various colors to paint. The brush tool has various brush shades and designs which can be used to give a different look to the line. The width tab specifies the thinness and thickness of the line. After selecting the shape, we can give more information with shape style which might has options like outline, filled, and many more. The edit menu provides options like cut, copy, and paste to edit images and drawings. The image menu has a clear button which is generally used to clears everything whatever still have done.

---
+++ Canvas/Paint/TextDialog.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Paint {
+  partial class TextDialog {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.fontBtn = new System.Windows.Forms.Button();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.drawBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 4);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Text:";
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(13, 27);
+            this.textBox.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox.Size = new System.Drawing.Size(488, 93);
+            this.textBox.TabIndex = 1;
+            // 
+            // fontBtn
+            // 
+            this.fontBtn.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fontBtn.Location = new System.Drawing.Point(13, 128);
+            this.fontBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.fontBtn.Name = "fontBtn";
+            this.fontBtn.Size = new System.Drawing.Size(100, 34);
+            this.fontBtn.TabIndex = 2;
+            this.fontBtn.Text = "&Font";
+            this.fontBtn.UseVisualStyleBackColor = true;
+            this.fontBtn.Click += new System.EventHandler(this.fontBtn_Click);
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelBtn.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cancelBtn.Location = new System.Drawing.Point(401, 128);
+            this.cancelBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(100, 34);
+            this.cancelBtn.TabIndex = 3;
+            this.cancelBtn.Text = "&Cancel";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            // 
+            // drawBtn
+            // 
+            this.drawBtn.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.drawBtn.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.drawBtn.Location = new System.Drawing.Point(293, 128);
+            this.drawBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.drawBtn.Name = "drawBtn";
+            this.drawBtn.Size = new System.Drawing.Size(100, 34);
+            this.drawBtn.TabIndex = 4;
+            this.drawBtn.Text = "&Draw";
+            this.drawBtn.UseVisualStyleBackColor = true;
+            // 
+            // TextDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 175);
+            this.Controls.Add(this.drawBtn);
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.fontBtn);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "TextDialog";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.TextBox textBox;
+    private System.Windows.Forms.Button fontBtn;
+    private System.Windows.Forms.Button cancelBtn;
+    private System.Windows.Forms.Button drawBtn;
+  }
+}
